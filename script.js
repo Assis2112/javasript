00951067868db3be1ea461048d092c63b4648ed4
Assis2112 <ghostzin172@gmail.com>
2026-03-17 10:22:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+//let number1 = 5
+//let number2 = 5
+//soma = number1 + number2
+//console.log(soma)
+//var nome = "Assis"
+
+//CONCATENAR
+//let userName = "Assis"
+//let email = " Assis22@gmail.com"
+
+//console.log(userName, email, "teste")
+
+//let message = "Olá, " + userName + " você conectou com o email " + email
+
+//console.log(message)
+
+//onsole.log(`Olá,  ${userName} você conectou com o email ${email}`)
+
+
+
+let numberOne = 20
+let numberTwo = 10
+
+let soma = numberOne + numberTwo
+let mult = numberOne * numberTwo
+let men = numberOne - numberTwo
+let div = numberOne / numberTwo
+let resto = numberOne % numberTwo
+let pot = numberOne ** numberTwo
+
+console.log(` A soma de ${numberOne} e ${numberTwo} é ${soma}`)
+console.log(` A mult de ${numberOne} e ${numberTwo} é ${mult}`)
+console.log(` A subtraçao de ${numberOne} e ${numberTwo} é ${men}`)
+console.log(` A divizao de ${numberOne} e ${numberTwo} é ${div}`)
+console.log(` A resto de ${numberOne} e ${numberTwo} é ${resto}`)
+console.log(` A potencia de ${numberOne} e ${numberTwo} é ${pot}`)
