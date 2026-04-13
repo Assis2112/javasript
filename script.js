42adb21a9e1464461d5204d81cfbf17048558ac4
Assis2112 <ghostzin172@gmail.com>
2026-04-13 07:56:08 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,19 +18,161 @@
 
 
 
-let numberOne = 20
-let numberTwo = 10
+//let numberOne = 20
+//let numberTwo = 10
 
-let soma = numberOne + numberTwo
-let mult = numberOne * numberTwo
-let men = numberOne - numberTwo
-let div = numberOne / numberTwo
-let resto = numberOne % numberTwo
-let pot = numberOne ** numberTwo
+//let soma = numberOne + numberTwo
+//let mult = numberOne * numberTwo
+//let men = numberOne - numberTwo
+//let div = numberOne / numberTwo
+//let resto = numberOne % numberTwo
+//let pot = numberOne ** numberTwo
 
-console.log(` A soma de ${numberOne} e ${numberTwo} é ${soma}`)
-console.log(` A mult de ${numberOne} e ${numberTwo} é ${mult}`)
-console.log(` A subtraçao de ${numberOne} e ${numberTwo} é ${men}`)
-console.log(` A divizao de ${numberOne} e ${numberTwo} é ${div}`)
-console.log(` A resto de ${numberOne} e ${numberTwo} é ${resto}`)
-console.log(` A potencia de ${numberOne} e ${numberTwo} é ${pot}`)
+//console.log(` A soma de ${numberOne} e ${numberTwo} é ${soma}`)
+//console.log(` A mult de ${numberOne} e ${numberTwo} é ${mult}`)
+//console.log(` A subtraçao de ${numberOne} e ${numberTwo} é ${men}`)
+//console.log(` A divizao de ${numberOne} e ${numberTwo} é ${div}`)
+//console.log(` A resto de ${numberOne} e ${numberTwo} é ${resto}`)
+//console.log(` A potencia de ${numberOne} e ${numberTwo} é ${pot}`)
+
+
+
+
+//let number = 40
+
+//number++
+//console.log(number)
+
+//AND &&
+//OR ||
+//NOT !
+
+//let email = true
+//let password = false
+
+//console.log( email && password)
+//console.log(email || password)
+//console.log(!email)
+
+
+//let idade = 17
+//let ingresso = true
+
+//if(idade>=20 || ingresso) {
+//  console.log("pode entar no evento!");
+//}else{
+//  console.log("Não pode entar.");
+//}
+
+//let horas = 12
+
+//if(horas <= 12){
+//console.log("Bom dia!")
+//}else if(horas >12 && horas <= 18){
+//console.log("Boa tarde!")
+//}else {
+//console.log("Boa noite!")
+//}
+
+//primeira
+//let numero = 4759278
+
+//let PoI= numero%2
+
+//if( PoI == 0){
+//console.log("Par")
+//}else if(PoI == 1){
+//console.log("Impar")
+//}
+
+
+
+//segunda
+
+//let number1 = 40
+//let number2 = 40
+
+//if( number1 == number2){
+//console.log("iguais")
+//}else if (number1 > number2){
+//console.log("maior")
+//}else{
+//console.log("menor")
+//}
+
+//terceira
+//let numero = 5
+//let numero2 = 1
+//let numero3 = 2
+//let numero4 = 3
+//let numero5 = 4
+//let numero6 = 5
+//let numero7 = 6
+//let numero8 = 7
+//let numero9 = 8
+//let numero10 = 9
+
+//resutado1 = numero * numero2
+//resutado2 = numero * numero3
+//resutado3 = numero * numero4
+//resutado4 = numero * numero5
+//resutado5 = numero * numero6
+//resutado6 = numero * numero7
+//resutado7 = numero * numero8
+//resutado8 = numero * numero9
+//resutado9 = numero * numero10
+
+
+//console.log(`5x1= ${resutado1}`)
+//console.log(`5x2= ${resutado2}`)
+//console.log(`5x3= ${resutado3}`)
+//console.log(`5x4= ${resutado4}`)
+//console.log(`5x5= ${resutado5}`)
+//console.log(`5x6= ${resutado6}`)
+//console.log(`5x7= ${resutado7}`)
+//console.log(`5x8= ${resutado8}`)
+//console.log(`5x9= ${resutado9}`)
+
+//for (let i = 1; i <= 100; i++) {
+  //  console.log("5 x " + i + "=" + (5 * i))
+//}
+
+//quarta
+
+//let number1= 8
+//let number2= 2
+//let pot = number1 ** number2
+//console.log(` A potencia de ${number1} e ${number2} é ${pot}`)
+
+//quinta
+
+//let n1 = 8
+//let n2 = 9
+//let n3 = 10
+//let media = (n1 + n2 + n3) / 3;
+//console.log("media", media);
+//if (media >=7){
+//console.log ("aprovado");
+//}else if (media >= 5){
+//console.log ("recuperação")
+//}else{
+//console.log("reprovado");
+//}
+
+//sexta
+
+//console.log (`10`)
+//console.log (`9`)
+//console.log (`8`)
+//console.log (`7`)
+//console.log (`6`)
+//console.log (`5`)
+//console.log (`4`)
+//console.log (`3`)
+//console.log (`2`)
+//console.log (`1`)
+//console.log (`0`)
+
+for (let i = 10; i >= 0; i--) {
+    console.log(i)
+}
